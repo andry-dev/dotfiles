38d88495f4d4a57cf06d5c22eff42b8ab622255a
andry-dev <andry-dev@users.noreply.github.com>
2022-11-27 12:27:27 +0100

    Themes: Make the evening start sooner

--- private_dot_config/nvim/pack/andry/start/common/lua/config/themes.lua
+++ private_dot_config/nvim/pack/andry/start/common/lua/config/themes.lua
@@ -1,7 +1,7 @@
 -- Normal theme configuration
 local default_theme_config = require 'themes'.setup {
     daystart = 7,
-    dayend = 18,
+    dayend = 16,
     light = 'nofrils-acme',
     dark = 'nofrils-dark',
 }
